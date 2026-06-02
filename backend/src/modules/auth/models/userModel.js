8f83238a5ab75b66455c8b6d6937256e5891f7ca
taslima <garryanderson337@gmail.com>
2026-06-02 16:20:05 +0600

    feat: create user Mongoose model with validation schema for authentication module

--- backend/src/modules/auth/models/userModel.js
+++ backend/src/modules/auth/models/userModel.js
@@ -2,31 +2,31 @@ import { request } from "express";
 import mongoose from "mongoose";
 import { validation } from "../../../shared/constant";
 
-const userSchema =new mongoose.Schema({
-    name : {
-        type : String,
-        required : [true, 'name is required'],
-        trim : true,
-        maxLength : [validation.nameMaxLength,`name cannt exceed ${validation.nameMaxLength} characters`]
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required'],
+        trim: true,
+        maxLength: [validation.nameMaxLength, `name cannt exceed ${validation.nameMaxLength} characters`]
     },
     email: {
-        type : String,
-        required : [true, 'email is required'],
-        trim : true,
+        type: String,
+        required: [true, 'email is required'],
+        trim: true,
         unique: true,
         lowercase: true,
-        match: [/^\s+@\s+\.\s+$/, 'please provide a valid email']
+        match: [/^\s+@\s+\.\s+$/, 'Please provide a valid email']
     },
     password: {
-   type: String,
-   required: [true, 'password is required'],
-   minLength: [validation.passMinLength, `password must be at least ${validation.passMinLength} characters`],
-   select: false
- }
+        type: String,
+        required: [true, 'password is required'],
+        minLength: [validation.passMinLength, `password must be at least ${validation.passMinLength} characters`],
+        select: false
+    }
 },
-{
-    timestamps: true,
-    versionKey: false
-})
+    {
+        timestamps: true,
+        versionKey: false
+    })
 
-export const User = mongoose.models.User || mongoose.models("User",userSchema)
+export const User = mongoose.models.User || mongoose.models("User", userSchema)
