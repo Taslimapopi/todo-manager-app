ff247cef9f481cc39b477b80cb78910be29d62d0
taslima <garryanderson337@gmail.com>
2026-06-13 18:12:41 +0600

    jwt secret added

--- backend/src/modules/auth/services/authService.js
+++ backend/src/modules/auth/services/authService.js
@@ -1,6 +1,10 @@
+import { generateAccessToken } from "../../../utils/jwt.js"
 import { createAuthRepository } from "../repositories/authRepository.js"
 
 export const createAuthService = (userRepository = createAuthRepository() ) => {
+      const generateTokenPair = async (userId) => {
+        const accessToken = generateAccessToken()
+    }
     return {
         register: async ({ name, email, password }) => {
             const user = userRepository.create({ name, email, password })
---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken'
+import { env } from '../config/env'
+
+export const generateAccessToken = async (userId) =>{
+ return jwt.sign({id : userId},env.JWT_ACCESS_SECRET)
+}
+
+export const verifyAccessToken  = token =>{
+    return jwt.verify(token, env.JWT_ACCESS_SECRET)
+}
