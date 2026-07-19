13812964efa5a6b43ff7f909a86fd2837219f512
taslima <garryanderson337@gmail.com>
2026-07-19 17:39:55 +0600

    feat: enhance TodoRepository with search functionality and improve error handling

--- backend/src/modules/todo/repositories/todoRepository.js
+++ backend/src/modules/todo/repositories/todoRepository.js
@@ -25,44 +25,84 @@ import { title_collation, Todo } from "../models/todoModel.js";
 //     }
 // }
 
-const default_sort = {createdAt : -1}
+const default_sort = { createdAt: -1 };
 
 export class TodoRepository {
-    constructor(model = Todo){
-
-        this.model = model
-
+  constructor(model = Todo) {
+    this.model = model;
+  }
+  async create(todoData) {
+    try {
+      return await this.model.create(todoData);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new Error("duplicate title");
+      }
     }
-    async create (todoData) {
-        try{
-         return   await this.model.create(todoData)
-        }catch(error){
-            if(error.code ===11000){
-                throw new Error('duplicate title')
-            }
-        }
-    }
+  }
 
-    async insertMany(todos){
-        return await this.model.insertMany(todos, {ordered  :false, collation: title_collation})
-    }
+  async insertMany(todos) {
+    return await this.model.insertMany(todos, {
+      ordered: false,
+      collation: title_collation,
+    });
+  }
 
-    async findWithPagination (query, {page, limit, sort = default_sort},search){
-        if (page <1 || limit < 1){ throw new ApiError(http_status.bad_request,`invalid pagination params page=${page} limit=${limit}`)}
-
-        skip = (page-1)*limit
-
-        if(search){
-            const matchQuery = {...query}
-            if(matchQuery.user){
-                matchQuery.user = toObjectId(matchQuery.user,'User Id')
-            }
-        }
-
-        const [todos,total] = await Promise.all([(await this.model.find(query)).toSorted(sort).skip(skip).limit(limit).lean(), this.model.countDocuments(query)])
-
-         return {todos : todos.map(toPlainObject),total}
+  async findWithPagination(
+    query,
+    { page, limit, sort = default_sort },
+    search,
+  ) {
+    if (page < 1 || limit < 1) {
+      throw new ApiError(
+        http_status.bad_request,
+        `invalid pagination params page=${page} limit=${limit}`,
+      );
     }
 
-   
+    skip = (page - 1) * limit;
+
+    if (search) {
+      const matchQuery = { ...query };
+      if (matchQuery.user) {
+        matchQuery.user = toObjectId(matchQuery.user, "User Id");
+      }
+
+      const searchStage = {
+        $search: {
+          index: "todo-autocomplete",
+          compound: {
+            should: [
+              {
+                autocomplete: {
+                  query: search,
+                  path: "title",
+                  fuzzy: { maxEdits: 1 },
+                },
+              },
+              {
+                autocomplete: {
+                  query: search,
+                  path: "description",
+                  fuzzy: { maxEdits: 1 },
+                },
+              },
+            ],
+            minimumShouldMatch: 1,
+          },
+        },
+      };
+    }
+
+    const [todos, total] = await Promise.all([
+      (await this.model.find(query))
+        .toSorted(sort)
+        .skip(skip)
+        .limit(limit)
+        .lean(),
+      this.model.countDocuments(query),
+    ]);
+
+    return { todos: todos.map(toPlainObject), total };
+  }
 }
